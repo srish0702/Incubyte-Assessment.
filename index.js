85c9055fe4446e437bee16d50d8d76173a3b579f
srish0702 <srishtidhamal8272@gmail.com>
2024-10-17 12:28:45 +0530

    Added test cases

--- index.js
+++ index.js
@@ -35,3 +35,15 @@ class StringCalculator {
 }
 
 
+// Testing Some Inputs
+const calculator = new StringCalculator();
+console.log(calculator.add("")); // Output: 0
+console.log(calculator.add("1")); // Output: 1
+console.log(calculator.add("1,5")); // Output: 6
+console.log(calculator.add("1\n2,3")); // Output: 6
+console.log(calculator.add("//;\n1;2")); // Output: 3
+try {
+    console.log(calculator.add("1,-2,3,-4")); // Throws error
+} catch (e) {
+    console.error(e.message); // Output: negative numbers not allowed: -2, -4
+}
